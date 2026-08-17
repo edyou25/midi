bbefc6e1c3e50df972f962d91b4502dacc991c72
edward.you <edward.you@polyu.edu.hk>
2026-08-17 13:55:54 +0800

    Refactor MIDI project structure: remove old files, add new configuration and plotting modules

--- play.py
+++ play.py
@@ -1,29 +1,103 @@
-import yaml
-import mido
+import time
+import threading
+
 import fluidsynth
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-with open("config.yaml") as f:
-    config = yaml.safe_load(f)
+from plot import plot_midi, load_config
 
-mid = mido.MidiFile(config["mid_path"])
 
-fs = fluidsynth.Synth()
-fs.start(driver="pulseaudio")
+config = load_config()
 
-sfid = fs.sfload(config["soundfont"])
+fig, ax, cursor, time_text, mid = plot_midi(
+    show=False,
+    cursor=True
+)
 
-# Default piano
-for ch in range(16):
-    fs.program_select(ch, sfid, 0, 0)
+duration = mid.length
 
-for msg in mid.play():
-    if msg.type == "note_on":
-        fs.noteon(msg.channel, msg.note, msg.velocity)
+state = {
+    "start": None,
+    "done": False
+}
 
-    elif msg.type == "note_off":
-        fs.noteoff(msg.channel, msg.note)
 
-    elif msg.type == "program_change":
-        fs.program_change(msg.channel, msg.program)
+def format_time(seconds):
+    m = int(seconds // 60)
+    s = int(seconds % 60)
+    return f"{m:02d}:{s:02d}"
 
-fs.delete()
+
+def play():
+    fs = fluidsynth.Synth()
+    fs.start(driver="pulseaudio")
+
+    sfid = fs.sfload(config["soundfont"])
+
+    for ch in range(16):
+        fs.program_select(ch, sfid, 0, 0)
+
+    state["start"] = time.monotonic()
+
+    for msg in mid.play():
+
+        if msg.is_meta:
+            continue
+
+        if msg.type == "note_on":
+            if msg.velocity > 0:
+                fs.noteon(msg.channel, msg.note, msg.velocity)
+            else:
+                fs.noteoff(msg.channel, msg.note)
+
+        elif msg.type == "note_off":
+            fs.noteoff(msg.channel, msg.note)
+
+        elif msg.type == "program_change":
+            fs.program_change(msg.channel, msg.program)
+
+        elif msg.type == "control_change":
+            fs.cc(msg.channel, msg.control, msg.value)
+
+    state["done"] = True
+
+    for ch in range(16):
+        fs.cc(ch, 123, 0)
+
+    fs.delete()
+
+
+def update(_):
+    if state["start"] is None:
+        return cursor, time_text
+
+    elapsed = min(
+        time.monotonic() - state["start"],
+        duration
+    )
+
+    cursor.set_xdata([elapsed, elapsed])
+
+    time_text.set_text(
+        f"{format_time(elapsed)} / {format_time(duration)}"
+    )
+
+    return cursor, time_text
+
+
+ani = FuncAnimation(
+    fig,
+    update,
+    interval=50,
+    cache_frame_data=False
+)
+
+thread = threading.Thread(
+    target=play,
+    daemon=True
+)
+
+thread.start()
+
+plt.show()
